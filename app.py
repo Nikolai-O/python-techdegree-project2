50c1829f0b6a50db9b3f099a4f1289e43611c9bf
Nikolai-O <70658689+Nikolai-O@users.noreply.github.com>
2020-09-29 10:34:34 -0400

    Update app.py
    Added try and except blocks to handle errors.

--- app.py
+++ app.py
@@ -81,79 +81,92 @@ def add_guardians(team):
 if __name__ == '__main__':
     clean_data(players_copy)
     balance_team(players_copy)
-    selection = 1
+    selection = 0
 
-    while selection == 1:
+    while selection != 2:
 
         print("BASKETBALL STATISTICS TOOL\n\n"+"-"*11+"MENU"+"-"*11+"\n\n Please enter your selection:\n  1) Display Team Statistics\n  2) Quit\n")
 
-        selection = int(input("Enter an option > "))
+        try:
+            selection = int(input("Enter an option > "))
+            if selection > 2 or selection < 1:
+                print("\n*ERROR* Please enter either 1 or 2 *ERROR*\n")
+        except Exception:
+            print("\n*ERROR* Please enter a *NUMBER* (either 1 or 2) *ERROR*\n")
 
-        if selection == 2:
-            break
-
-        else:
+        if selection == 1:
             print("\n1) Panthers\n2) Bandits\n3) Warriors\n")
+            team_selection = 0
+            while not team_selection:
 
-            team_selection = int(input("Enter an option > "))
+                try:
+                    team_selection = int(input("Enter an option > "))
+                    if team_selection > 3 or team_selection < 1:
+                        print("\n*ERROR* Please enter either 1, 2 or 3 *ERROR*\n")
+                        team_selection = 0
+                except Exception:
+                    print("\n*ERROR* Please enter a *NUMBER* (either 1, 2 or 3) *ERROR*\n")
+                    team_selection = 0
+                if team_selection == 1:
+                    experienced_players = experienced(Panthers)
+                    inexperienced_players = inexperienced(Panthers)
+                    average_height = calc_average_height(Panthers)
+                    guardians_list = add_guardians(Panthers)
 
-            if team_selection == 1:
-                experienced_players = experienced(Panthers)
-                inexperienced_players = inexperienced(Panthers)
-                average_height = calc_average_height(Panthers)
-                guardians_list = add_guardians(Panthers)
-
-                print("""\nTeam: Panthers
+                    print("""\nTeam: Panthers
 --------------------\n
 Total players: {}
 Experienced players: {}
 Inexperienced players: {}
 Average height: {}\n
 Players on team:""".format(len(Panthers), format(experienced_players), format(inexperienced_players), format(average_height)))
-                panthers_players = []
-                for player in Panthers:
-                    panthers_players.append(str(player['name']))
-                print(f"  {panthers_players[0]}, {panthers_players[1]}, {panthers_players[2]}, {panthers_players[3]}, {panthers_players[4]}, {panthers_players[5]}\n")
-                print("Guardians:\n  {}\n".format(guardians_list))
+                    panthers_players = []
+                    for player in Panthers:
+                        panthers_players.append(str(player['name']))
+                    print(f"  {panthers_players[0]}, {panthers_players[1]}, {panthers_players[2]}, {panthers_players[3]}, {panthers_players[4]}, {panthers_players[5]}\n")
+                    print("Guardians:\n  {}\n".format(guardians_list))
+                    input("Press ENTER to continue... \n")
 
-            elif team_selection == 2:
-                experienced_players = experienced(Bandits)
-                inexperienced_players = inexperienced(Bandits)
-                average_height = calc_average_height(Bandits)
-                guardians_list = add_guardians(Bandits)
+                elif team_selection == 2:
+                    experienced_players = experienced(Bandits)
+                    inexperienced_players = inexperienced(Bandits)
+                    average_height = calc_average_height(Bandits)
+                    guardians_list = add_guardians(Bandits)
 
-                print("""\nTeam: Bandits
+                    print("""\nTeam: Bandits
 --------------------\n
 Total players: {}
 Experienced players: {}
 Inexperienced players: {}
 Average height: {}\n
 Players on team:""".format(len(Bandits), format(experienced_players), format(inexperienced_players), format(average_height)))
-                bandits_players = []
-                for player in Bandits:
-                    bandits_players.append(str(player['name']))
-                print(f"  {bandits_players[0]}, {bandits_players[1]}, {bandits_players[2]}, {bandits_players[3]}, {bandits_players[4]}, {bandits_players[5]}\n")
-                print("Guardians:\n  {}\n".format(guardians_list))
+                    bandits_players = []
+                    for player in Bandits:
+                        bandits_players.append(str(player['name']))
+                    print(f"  {bandits_players[0]}, {bandits_players[1]}, {bandits_players[2]}, {bandits_players[3]}, {bandits_players[4]}, {bandits_players[5]}\n")
+                    print("Guardians:\n  {}\n".format(guardians_list))
+                    input("Press ENTER to continue... \n")
 
-            elif team_selection == 3:
-                experienced_players = experienced(Warriors)
-                inexperienced_players = inexperienced(Warriors)
-                average_height = calc_average_height(Warriors)
-                guardians_list = add_guardians(Warriors)
+                elif team_selection == 3:
+                    experienced_players = experienced(Warriors)
+                    inexperienced_players = inexperienced(Warriors)
+                    average_height = calc_average_height(Warriors)
+                    guardians_list = add_guardians(Warriors)
 
-                print("""\nTeam: Warriors
+                    print("""\nTeam: Warriors
 --------------------\n
 Total players: {}
 Experienced players: {}
 Inexperienced players: {}
 Average height: {}\n
 Players on team:""".format(len(Warriors), format(experienced_players), format(inexperienced_players), format(average_height)))
-                warriors_players = []
-                for player in Warriors:
-                    warriors_players.append(str(player['name']))
-                print(f"  {warriors_players[0]}, {warriors_players[1]}, {warriors_players[2]}, {warriors_players[3]}, {warriors_players[4]}, {warriors_players[5]}\n")
-                print("Guardians:\n  {}\n".format(guardians_list))
+                    warriors_players = []
+                    for player in Warriors:
+                        warriors_players.append(str(player['name']))
+                    print(f"  {warriors_players[0]}, {warriors_players[1]}, {warriors_players[2]}, {warriors_players[3]}, {warriors_players[4]}, {warriors_players[5]}\n")
+                    print("Guardians:\n  {}\n".format(guardians_list))
 
-            input("Press ENTER to continue... \n")
+                    input("Press ENTER to continue... \n")
 
     print("\nThank you for using BASKETBALL STATISTICS TOOL\n")
+
